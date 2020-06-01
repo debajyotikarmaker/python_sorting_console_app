e23507df3bdb5602eed197fb84c057d1133dc750
debajyotikarmaker <uqdkarma@gmail.com>
2020-06-02 04:44:08 +0600

    bubble sort variations added

---
+++ bubble_sort/bubble_sort.py
@@ -0,0 +1,69 @@
+def dummy_bubble_sort(arr):
+    """[summary]
+
+    Arguments:
+        arr {[type]} -- [description]
+    """
+    print("Starting Dummy Bubble Sort ...")
+
+    dashLen = calc_dash_length(arr)
+    for i in range(len(arr)-1):
+        print("i=", i, end="")
+        for j in range(len(arr)-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            print("\tj=", j, "\tarr=", arr)
+        printHorizontalLine(dashLen)
+    print("Sorted List: ", arr)
+
+
+def traditional_bubble_sort(arr):
+    """[summary]
+
+    Arguments:
+        arr {[type]} -- [description]
+    """
+
+    print("Starting Traditional Bubble Sort ...")
+    dashLen = calc_dash_length(arr)
+    for i in range(len(arr)-1):
+        print("i=", i, end="")
+        for j in range(len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            print("\tj=", j, "\tarr=", arr[0:len(arr)-i-1])
+        printHorizontalLine(dashLen)
+    print("Sorted List: ", arr)
+
+
+def optimized_bubble_sort(arr):
+    """[summary]
+
+    Arguments:
+        arr {[type]} -- [description]
+    """
+
+    print("Starting Optimized Bubble Sort ...")
+    dashLen = calc_dash_length(arr)
+    for i in range(len(arr)-1):
+        print("i=", i, end="")
+        swapped = False
+        for j in range(len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+            print("\tj=", j, "\tarr=", arr[0:len(arr)-i-1])
+        printHorizontalLine(dashLen)
+        if swapped == False:
+            break
+    print("Sorted List: ", arr)
+
+
+def printHorizontalLine(numOfitems):
+    for _ in range(numOfitems):
+        print("_", end="")
+    print()
+
+
+def calc_dash_length(arr):
+    return 21 + sum(len(str(k)) for k in arr) + len(arr)*2
--- bubble_sort/main.py
+++ bubble_sort/main.py
@@ -1,10 +1,27 @@
-from bubble import bubble_sort
+from random import randint
+from time import time
+
+from bubble_sort import dummy_bubble_sort
+from bubble_sort import traditional_bubble_sort
+from bubble_sort import optimized_bubble_sort
+
 
 data = []
-numOfItems = int(input("Enter the number of inputs: "))
-for i in range(numOfItems):
-    print(f"Enter {i} item: ", end="")
-    data.append(int(input()))
+# choice = int(
+#     input("Select Input Method: \n (1) Manual Entry \n (2) Randomized\n Enter your choioce: "))
+# if choice == 1:
+#     numOfItems = int(input("Enter the number of inputs: "))
+#     for i in range(numOfItems):
+#         print(f"Enter {i} item: ", end="")
+#         data.append(int(input()))
+# elif choice == 2:
+#     numOfItems = int(input("Enter the number of inputs: "))
+#     for i in range(numOfItems):
+#         data.append(randint(1, 1000))
 
-print(data)
-bubble_sort(data)
+
+data = [5, 3, 1, 9, 8, 2, 4, 7]
+print("Input data: ", data)
+dummy_bubble_sort(data)
+# traditional_bubble_sort(data)
+# optimized_bubble_sort(data)
