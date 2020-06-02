14457e2284089afd0412c39ae92152898b5204c1
debajyotikarmaker <uqdkarma@gmail.com>
2020-06-02 15:23:11 +0600

    gitignore added

--- bubble_sort/main.py
+++ bubble_sort/main.py
@@ -7,20 +7,19 @@ from bubble_sort import optimized_bubble_sort
 
 
 data = []
-# choice = int(
-#     input("Select Input Method: \n (1) Manual Entry \n (2) Randomized\n Enter your choioce: "))
-# if choice == 1:
-#     numOfItems = int(input("Enter the number of inputs: "))
-#     for i in range(numOfItems):
-#         print(f"Enter {i} item: ", end="")
-#         data.append(int(input()))
-# elif choice == 2:
-#     numOfItems = int(input("Enter the number of inputs: "))
-#     for i in range(numOfItems):
-#         data.append(randint(1, 1000))
+choice = int(
+    input("Select Input Method: \n (1) Manual Entry \n (2) Randomized\n Enter your choioce: "))
+if choice == 1:
+    numOfItems = int(input("Enter the number of inputs: "))
+    for i in range(numOfItems):
+        print(f"Enter {i} item: ", end="")
+        data.append(int(input()))
+elif choice == 2:
+    numOfItems = int(input("Enter the number of inputs: "))
+    for i in range(numOfItems):
+        data.append(randint(1, 1000))
 
 
-data = [5, 3, 1, 2, 4]
 print("Input data: ", data)
 # dummy_bubble_sort(data)
 traditional_bubble_sort(data)
